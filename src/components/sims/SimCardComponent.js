9a1e4b8632060fdcc5ece0e1ca405a247b728ddf
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-01-11 16:20:44 +0100

    Improve agent details

--- src/components/sims/SimCardComponent.js
+++ src/components/sims/SimCardComponent.js
@@ -1,43 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from "prop-types";
 
+import FormModalComponent from "../modals/FormModalComponent";
+import AgentDetailsContainer from "../../containers/agents/AgentDetailsContainer";
 import {AGENT_COLLECTOR_TYPE, COLLECTOR_TYPE} from "../../constants/typeConstants";
 import {dateToString, formatNumber, upperFirstCase} from "../../functions/generalFunctions";
 
 // Component
 function SimCardComponent({sim}) {
+    // Local states
+    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT/RESSOURCE", id: ''});
+
+    // Hide agent details modal form
+    const handleAgentDetailsModalHide = () => {
+        setAgentDetailsModal({...agentDetailsModal, show: false})
+    }
+
     // Render
     return (
-        <ul className="list-group list-group-unbordered">
-            <li className="list-group-item">
-                <b>Créer le</b>
-                <span className="float-right">{dateToString(sim.creation)}</span>
-            </li>
-            <li className="list-group-item">
-                <b>Nom</b>
-                <span className="float-right">{sim.name}</span>
-            </li>
-            <li className="list-group-item">
-                <b>Numéro</b>
-                <span className="float-right">{sim.number}</span>
-            </li>
-            <li className="list-group-item">
-                <b>Solde flotte</b>
-                <span className="float-right text-success text-bold">{formatNumber(sim.balance)}</span>
-            </li>
-            <li className="list-group-item">
-                <b>Opérateur</b>
-                <span className="float-right">{sim.operator.name}</span>
-            </li>
-            {AGENT_COLLECTOR_TYPE.includes(sim.type.name) &&
+        <div>
+            <ul className="list-group list-group-unbordered">
                 <li className="list-group-item">
-                    <b>{upperFirstCase(sim.type.name)}</b>
-                    <span className="float-right">
-                        {sim.type.name === COLLECTOR_TYPE ? sim.collector.name : sim.agent.name}
-                    </span>
+                    <b>Créer le</b>
+                    <span className="float-right">{dateToString(sim.creation)}</span>
                 </li>
-            }
-        </ul>
+                <li className="list-group-item">
+                    <b>Nom</b>
+                    <span className="float-right">{sim.name}</span>
+                </li>
+                <li className="list-group-item">
+                    <b>Numéro</b>
+                    <span className="float-right">{sim.number}</span>
+                </li>
+                <li className="list-group-item">
+                    <b>Solde flotte</b>
+                    <span className="float-right text-success text-bold">{formatNumber(sim.balance)}</span>
+                </li>
+                <li className="list-group-item">
+                    <b>Opérateur</b>
+                    <span className="float-right">{sim.operator.name}</span>
+                </li>
+                {AGENT_COLLECTOR_TYPE.includes(sim.type.name) &&
+                    <li className="list-group-item">
+                        <b>{upperFirstCase(sim.type.name)}</b>
+                        <span className="float-right">
+                            {sim.type.name === COLLECTOR_TYPE
+                                ? sim.collector.name
+                                : (
+                                    <>
+                                        {sim.agent.name}
+                                        <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                           onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: sim.agent.id})}
+                                        />
+                                    </>
+                                )
+                            }
+                        </span>
+                    </li>
+                }
+            </ul>
+            {/* Modal */}
+            <FormModalComponent modal={agentDetailsModal} handleClose={handleAgentDetailsModalHide}>
+                <AgentDetailsContainer id={agentDetailsModal.id} />
+            </FormModalComponent>
+        </div>
     )
 }
 
