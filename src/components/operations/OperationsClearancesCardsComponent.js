6df5e6ccd258b8761f89cb04e8afcc36884e4da4
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-01-27 13:31:13 +0100

    Save changes

---
+++ src/components/operations/OperationsClearancesCardsComponent.js
@@ -0,0 +1,121 @@
+import PropTypes from "prop-types";
+import React, {useState} from 'react';
+
+import LoaderComponent from "../LoaderComponent";
+import {DONE} from "../../constants/typeConstants";
+import FormModalComponent from "../modals/FormModalComponent";
+import {dateToString, formatNumber} from "../../functions/generalFunctions";
+import SimDetailsContainer from "../../containers/sims/SimDetailsContainer";
+import AgentDetailsContainer from "../../containers/agents/AgentDetailsContainer";
+
+// Component
+function OperationsClearancesCardsComponent({refuels, handleConfirmModalShow}) {
+    // Local states
+    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT/RESSOURCE", id: ''});
+    const [incomingSimDetailsModal, setIncomingSimDetailsModal] = useState({show: false, header: 'DETAIL DE LA PUCE AGENT', id: ''});
+    const [outgoingSimDetailsModal, setOutgoingSimDetailsModal] = useState({show: false, header: 'DETAIL DE LA PUCE DE FLOTTAGE', id: ''});
+
+    // Hide incoming sim details modal form
+    const handleIncomingSimDetailModalHide = () => {
+        setIncomingSimDetailsModal({...incomingSimDetailsModal, show: false})
+    }
+
+    // Hide outgoing sim details modal form
+    const handleOutgoingSimDetailModalHide = () => {
+        setOutgoingSimDetailsModal({...outgoingSimDetailsModal, show: false})
+    }
+
+    // Hide agent details modal form
+    const handleAgentDetailsModalHide = () => {
+        setAgentDetailsModal({...agentDetailsModal, show: false})
+    }
+
+    // Render
+    return (
+        <>
+            <div className="row m-1">
+                {refuels.map((item, key) => {
+                    return (
+                        <div className="col-lg-4 col-md-6" key={key}>
+                            <div className="card">
+                                <div className={`card-header ${item.status === DONE ? 'bg-secondary' : 'bg-primary'}`}>
+                                    <h3 className="card-title text-bold">
+                                        <i className="fa fa-phone" /> {formatNumber(item.amount)}
+                                    </h3>
+                                </div>
+                                <div className="card-body">
+                                    <ul className="list-group list-group-unbordered">
+                                        <li className="list-group-item">
+                                            <b>Créer le</b>
+                                            <span className="float-right">{dateToString(item.creation)}</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <b>Agent/Ressource</b>
+                                            <span className="float-right">
+                                                {item.agent.name}
+                                                <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                                   onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: item.agent.id})}
+                                                />
+                                            </span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <b>Puce émetrice</b>
+                                            <span className="float-right">
+                                                {item.sim_outgoing.number}
+                                                <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                                   onClick={() => setOutgoingSimDetailsModal({...outgoingSimDetailsModal, show: true, id: item.sim_outgoing.id})}
+                                                />
+                                            </span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <b>Puce receptrice</b>
+                                            <span className="float-right">
+                                                {item.sim_incoming.number}
+                                                <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                                   onClick={() => setIncomingSimDetailsModal({...incomingSimDetailsModal, show: true, id: item.sim_incoming.id})}
+                                                />
+                                            </span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <b>Reste récouvrir</b>
+                                            <span className="float-right text-danger text-bold">{formatNumber(item.remaining)}</span>
+                                        </li>
+                                        <li className="list-group-item">
+                                            <b>Gestionaire</b>
+                                            <span className="float-right">{item.supplier.name}</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+                {refuels.length === 0 &&
+                    <div className="col-12">
+                        <div className='alert custom-active text-center'>
+                            Pas de flottages
+                        </div>
+                    </div>
+                }
+            </div>
+            {/* Modal */}
+            <FormModalComponent small={true} modal={incomingSimDetailsModal} handleClose={handleIncomingSimDetailModalHide}>
+                <SimDetailsContainer id={incomingSimDetailsModal.id} />
+            </FormModalComponent>
+            <FormModalComponent small={true} modal={outgoingSimDetailsModal} handleClose={handleOutgoingSimDetailModalHide}>
+                <SimDetailsContainer id={outgoingSimDetailsModal.id} />
+            </FormModalComponent>
+            <FormModalComponent modal={agentDetailsModal} handleClose={handleAgentDetailsModalHide}>
+                <AgentDetailsContainer id={agentDetailsModal.id} />
+            </FormModalComponent>
+        </>
+    )
+}
+
+// Prop types to ensure destroyed props data type
+OperationsClearancesCardsComponent.propTypes = {
+    clearances: PropTypes.array.isRequired,
+    handleConfirmModalShow: PropTypes.func.isRequired,
+};
+
+export default React.memo(OperationsClearancesCardsComponent);
---
+++ src/pages/operations/OperationsClearancesPage.js
@@ -0,0 +1,176 @@
+import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import HeaderComponent from "../../components/HeaderComponent";
+import LoaderComponent from "../../components/LoaderComponent";
+import AppLayoutContainer from "../../containers/AppLayoutContainer";
+import ErrorAlertComponent from "../../components/ErrorAlertComponent";
+import TableSearchComponent from "../../components/TableSearchComponent";
+import FormModalComponent from "../../components/modals/FormModalComponent";
+import {OPERATIONS_CLEARANCES_PAGE} from "../../constants/pageNameConstants";
+import ConfirmModalComponent from "../../components/modals/ConfirmModalComponent";
+import OperationsClearancesCardsComponent from "../../components/operations/OperationsClearancesCardsComponent";
+import {
+    dateToString,
+    formatNumber,
+    needleSearch,
+    requestFailed,
+    requestLoading,
+} from "../../functions/generalFunctions";
+
+// Component
+function OperationsClearancesPage({refuels, refuelsRequests, hasMoreData, page, dispatch, location}) {
+    // Local states
+    const [needle, setNeedle] = useState('');
+    const [confirmModal, setConfirmModal] = useState({show: false, body: '', id: 0});
+    const [refuelModal, setRefuelModal] = useState({show: false, header: 'EFFECTUER UN DESTOCKAGE'});
+
+    // Local effects
+    useEffect(() => {
+        // dispatch(emitSuppliesFetch());
+        // dispatch(emitAllSimsFetch());
+        // dispatch(emitAllAgentsFetch());
+        // Cleaner error alert while component did unmount without store dependency
+        return () => {
+            shouldResetErrorData();
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    const handleNeedleInput = (data) => {
+        setNeedle(data)
+    }
+
+    // Reset error alert
+    const shouldResetErrorData = () => {
+        // dispatch(storeSuppliesRequestReset());
+        // dispatch(storeNextSuppliesRequestReset());
+    };
+
+    // Fetch next refuels data to enhance infinite scroll
+    const handleNextRefuelsData = () => {
+        // dispatch(emitNextSuppliesFetch({page}));
+    }
+
+    // Show refuel modal form
+    const handleRefuelModalShow = (item) => {
+        setRefuelModal({...refuelModal, item, show: true})
+    }
+
+    // Hide refuel modal form
+    const handleRefuelModalHide = () => {
+        setRefuelModal({...refuelModal, show: false})
+    }
+
+    // Show confirm modal form
+    const handleConfirmModalShow = ({id, amount}) => {
+        setConfirmModal({...confirmModal, id, body: `Confirmer le déstockage de ${formatNumber(amount)}?`, show: true})
+    }
+
+    // Hide confirm modal form
+    const handleConfirmModalHide = () => {
+        setConfirmModal({...confirmModal, show: false})
+    }
+
+    // Trigger when clearance confirm confirmed on modal
+    const handleConfirm = (id) => {
+        handleConfirmModalHide();
+        // dispatch(emitConfirmReturn({id}));
+    };
+
+    // Render
+    return (
+        <>
+            <AppLayoutContainer pathname={location.pathname}>
+                <div className="content-wrapper">
+                    <HeaderComponent title={OPERATIONS_CLEARANCES_PAGE} icon={'fa fa-rss-square'} />
+                    <section className="content">
+                        <div className='container-fluid'>
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="card custom-card-outline">
+                                        {/* Search input */}
+                                        <div className="card-header">
+                                            <div className="card-tools">
+                                                <TableSearchComponent needle={needle} handleNeedle={handleNeedleInput} />
+                                            </div>
+                                        </div>
+                                        <div className="card-body">
+                                            {/* Error message */}
+                                            {requestFailed(refuelsRequests.list) && <ErrorAlertComponent message={refuelsRequests.list.message} />}
+                                            {requestFailed(refuelsRequests.next) && <ErrorAlertComponent message={refuelsRequests.next.message} />}
+                                            <button type="button"
+                                                    className="btn btn-theme mb-2"
+                                                    onClick={handleRefuelModalShow}
+                                            >
+                                                <i className="fa fa-plus" /> Effectuer un déstockage
+                                            </button>
+                                            {/* Search result & Infinite scroll */}
+                                            {(needle !== '' && needle !== undefined)
+                                                ? <OperationsClearancesCardsComponent supplies={searchEngine(refuels, needle)}
+                                                                                      handleConfirmModalShow={handleConfirmModalShow}
+                                                />
+                                                : (requestLoading(refuelsRequests.list) ? <LoaderComponent /> :
+                                                        <InfiniteScroll hasMore={hasMoreData}
+                                                                        dataLength={refuels.length}
+                                                                        loader={<LoaderComponent />}
+                                                                        next={handleNextRefuelsData}
+                                                                        style={{ overflow: 'hidden' }}
+                                                        >
+                                                            <OperationsClearancesCardsComponent supplies={refuels}
+                                                                                                handleConfirmModalShow={handleConfirmModalShow}
+                                                            />
+                                                        </InfiniteScroll>
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </AppLayoutContainer>
+            {/* Modal */}
+            <ConfirmModalComponent modal={confirmModal}
+                                   handleModal={handleConfirm}
+                                   handleClose={handleConfirmModalHide}
+            />
+            <FormModalComponent modal={refuelModal} handleClose={handleRefuelModalHide}>
+                {/*<OperationsFleetsAddSupplyContainer handleClose={handleRefuelModalHide} />*/}
+            </FormModalComponent>
+        </>
+    )
+}
+
+// Search engine
+function searchEngine(data, _needle) {
+    // Avoid empty filtering
+    if(_needle !== '' && _needle !== undefined) {
+        // Filter
+        data = data.filter((item) => {
+            return (
+                needleSearch(item.amount, _needle) ||
+                needleSearch(item.sim.number, _needle) ||
+                needleSearch(item.agent.name, _needle) ||
+                needleSearch(item.collector.name, _needle) ||
+                needleSearch(dateToString(item.creation), _needle)
+            )
+        });
+    }
+    // Return data
+    return data;
+}
+
+// Prop types to ensure destroyed props data type
+OperationsClearancesPage.propTypes = {
+    page: PropTypes.number.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    refuels: PropTypes.array.isRequired,
+    location: PropTypes.object.isRequired,
+    hasMoreData: PropTypes.bool.isRequired,
+    refuelsRequests: PropTypes.object.isRequired,
+};
+
+export default React.memo(OperationsClearancesPage);
